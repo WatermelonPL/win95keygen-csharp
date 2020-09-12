7aa91a275606da1364ba0cbbbfcfdc7c3bfe51a6
WatermelonPL <31414969+WatermelonPL@users.noreply.github.com>
2020-09-12 16:05:53 +0200

    Add project files.

---
+++ win95keygen-csharp/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace win95keygen-csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 33);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(391, 31);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Czekam...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generuj klucze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(125, 74);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Generuj klucze OEM";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(391, 339);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(274, 7);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(129, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ilość kluczy";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(319, 74);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(40, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "EN";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(365, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(38, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "PL";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Generator kluczy Windows 95 / Windows NT 4 / Office 95";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ win95keygen-csharp/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace win95keygen-csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] temp = new int[6];
+        int[] temp3 = new int[7];
+        int temp2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Inicjalizuj generator liczb losowych
+            Random rnd = new Random();
+            //Czyść obecną bazę kluczy
+            richTextBox1.Text = "";
+            progressBar1.Value = 0;
+            if (checkBox1.Checked)
+            {
+                progressBar1.Maximum = Convert.ToInt32(numericUpDown1.Value) * 4;
+                for (int i = 0; i < Convert.ToInt32(numericUpDown1.Value); i++)
+                {
+                    progressBar1.Value += 1;
+                    if (!radioButton1.Checked)
+                    {
+                        label1.Text = "Generuję klucz " + i.ToString() + "...";
+                    }
+                    else
+                    {
+                        label1.Text = "Generating key no. " + i.ToString() + "...";
+                    }
+                    string first = rnd.Next(0, 366).ToString("000") + rnd.Next(95, 99).ToString();
+                    progressBar1.Value += 1;
+                    do
+                    {
+                        for (int b = 0; b < temp.Length; b++)
+                        {
+                            temp[b] = rnd.Next(0, 9);
+                        }
+                    }
+                    while (temp.Sum() % 7 != 0 || temp[5] == 0 || temp[5] == 8 || temp[5] == 9);
+                    string second = "0" + temp[0].ToString() + temp[1].ToString() + temp[2].ToString() + temp[3].ToString() + temp[4].ToString() + temp[5].ToString();
+                    progressBar1.Value += 1;
+                    string third = rnd.Next(0, 99999).ToString("00000");
+                    progressBar1.Value += 1;
+                    richTextBox1.Text += first + "-OEM-" + second + "-" + third + "\r\n";
+                }
+            }
+            else
+            {
+                progressBar1.Maximum = Convert.ToInt32(numericUpDown1.Value) * 3;
+                for (int i = 0; i < Convert.ToInt32(numericUpDown1.Value); i++)
+                {
+                    progressBar1.Value += 1;
+                    if(!radioButton1.Checked)
+                    {
+                        label1.Text = "Generuję klucz " + i.ToString() + "...";
+                    } else
+                    {
+                        label1.Text = "Generating key no. " + i.ToString() + "...";
+                    }
+                    do
+                    {
+                        temp2 = rnd.Next(0, 999);
+                    }
+                    while (temp2 == 333 || temp2 == 444 || temp2 == 555 || temp2 == 666 || temp2 == 777 || temp2 == 888 || temp2 == 999);
+                    string second = temp2.ToString("000");
+                    progressBar1.Value += 1;
+                    do
+                    {
+                        for (int b = 0; b < temp3.Length; b++)
+                        {
+                            temp3[b] = rnd.Next(0, 9);
+                        }
+                    }
+                    while (temp3.Sum() % 7 != 0 || temp3[6] == 0 || temp3[6] == 8 || temp3[6] == 9);
+                    string third = temp3[0].ToString() + temp3[13. ].ToString() + temp3[2].ToString() + temp3[3].ToString() + temp3[4].ToString() + temp3[5].ToString() + temp3[6].ToString();
+                    progressBar1.Value += 1;
+                    richTextBox1.Text += second + "-" + third + "\r\n";
+                }
+            }
+            if (!radioButton1.Checked)
+            {
+                label1.Text = "Gotowe!";
+            }
+            else
+            {
+                label1.Text = "Done!";
+            }
+        }
+
+        public int Add(int addone, int addtwo)
+        {
+            return addone + addtwo;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(Properties.Settings.Default.lang == "en")
+            {
+                radioButton1.Checked = true;
+            } else if (Properties.Settings.Default.lang == "pl")
+            {
+                radioButton2.Checked = true;
+            }
+            if (!radioButton1.Checked)
+            {
+                label1.Text = "Czekam...";
+                this.Text = "Generator kluczy Windows 95 / Windows NT 4 / Office 95";
+                button1.Text = "Generuj klucze";
+                checkBox1.Text = "Generuj klucze OEM";
+            }
+            else
+            {
+                label1.Text = "Ready";
+                this.Text = "Windows 95 / Windows NT 4 / Office 95 keygen";
+                button1.Text = "Generate keys";
+                checkBox1.Text = "Generate OEM keys";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                Properties.Settings.Default.lang = "en";
+                Properties.Settings.Default.Save();
+            }
+            if (!radioButton1.Checked)
+            {
+                label1.Text = "Czekam...";
+                this.Text = "Generator kluczy Windows 95 / Windows NT 4 / Office 95";
+                button1.Text = "Generuj klucze";
+                checkBox1.Text = "Generuj klucze OEM";
+                label2.Text = "Liczba kluczy";
+            }
+            else
+            {
+                label1.Text = "Ready";
+                this.Text = "Windows 95 / Windows NT 4 / Office 95 keygen";
+                button1.Text = "Generate keys";
+                checkBox1.Text = "Generate OEM keys";
+                label2.Text = "Number of keys";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                Properties.Settings.Default.lang = "pl";
+                Properties.Settings.Default.Save();
+            }
+            if (!radioButton1.Checked)
+            {
+                label1.Text = "Czekam...";
+                this.Text = "Generator kluczy Windows 95 / Windows NT 4 / Office 95";
+                button1.Text = "Generuj klucze";
+                checkBox1.Text = "Generuj klucze OEM";
+            }
+            else
+            {
+                label1.Text = "Ready";
+                this.Text = "Windows 95 / Windows NT 4 / Office 95 keygen";
+                button1.Text = "Generate keys";
+                checkBox1.Text = "Generate OEM keys";
+            }
+        }
+    }
+}
